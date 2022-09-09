49e96aecfbdd38d5f42ec96db044e51e3bcfe5c3
Yogesh Kumar <101107688+Yogesh9973@users.noreply.github.com>
2022-09-09 15:17:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const localStorageData = () => {
+  let item = localStorage.getItem("id");
+
+  if (item) {
+    return JSON.parse(item);
+  } else return [];
+};
+
+const App = () => {
+  const [inputVal, setInputVal] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setData([...data, inputVal]);
+    setInputVal("");
+  };
+  const handleChange = (event) => {
+    setInputVal(event.target.value);
+  };
+  const handleDelete = (id) => {
+    let list = data.filter((cur, index) => {
+      return index !== id;
+    });
+
+    setData(list);
+  };
+  useEffect(() => {
+    // console.log(data)
+    localStorage.setItem("id", JSON.stringify(data));
+  }, [data]);
+
+  useEffect(() => {
+    setData(localStorageData());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1> Todo List</h1>
+        <input
+          type="text"
+          name="text"
+          placeholder="Enter Item"
+          onChange={handleChange}
+          value={inputVal}
+        />
+        <button type="submit">ADD</button>
+        <ol>
+          {data.map((val, index) => {
+            return (
+              <li key={val + index}>
+                <span>
+                  <div>{val}</div>
+                  <button onClick={() => handleDelete(index)}>Delete</button>
+                </span>
+              </li>
+            );
+          })}
+        </ol>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
